8bd5df2e212e7fb66f28048e6113ee6fd9e1c2aa
bharadwaj <bharadwajpulikanti11@gmail.com>
2025-08-02 07:29:17 +0530

    Update __init__.py

--- cognize/__init__.py
+++ cognize/__init__.py
@@ -1,4 +1,15 @@
 from .epistemic import EpistemicState
 from .policies import policies
-
-__all__ = ["EpistemicState", "policies"]
+# Common symbolic policies (minimal set)
+from .policies import (
+    realign_tanh_fn,
+    threshold_adaptive_fn,
+    collapse_soft_decay_fn
+)
+__all__ = [
+    "EpistemicState",
+    "policies",
+    "realign_tanh_fn",
+    "threshold_adaptive_fn",
+    "collapse_soft_decay_fn"
+]
